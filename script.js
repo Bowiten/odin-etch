2e6d07b8cbc297e39d98e6bf5391310735871b3b
Bowiten <71778489+Bowiten@users.noreply.github.com>
2023-06-30 17:18:54 -0700

    Implement new grid button

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const grid = document.querySelector('#grid');
+const newGrid = document.querySelector('#new-grid');
+
+newGrid.addEventListener('click', () => {
+    clearGrid();
+    let gridSize = 0;
+    do {
+        let p = prompt('Enter the grid size here (must be greater than 0 and less than 100):')
+        gridSize = p;
+    }
+    while (gridSize < 0 || gridSize > 100)
+    grid.style.width = gridSize * 30 + 'px';
+    createGrid(gridSize);
+    addMouseover();
+});
 
 function addCell() {
     const cell = document.createElement('div');
@@ -7,8 +21,8 @@ function addCell() {
 }
 
 function addMouseover() {
-    const cell = document.querySelectorAll('.cell');
-    cell.forEach(cell => {cell.addEventListener('mouseover', () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {cell.addEventListener('mouseover', () => {
             cell.style.backgroundColor = 'black';
         })
     });
@@ -18,6 +32,11 @@ function createGrid(gridSize) {
     for (i = 0; i < gridSize ** 2; i++) {
         addCell();
     }
+}
+
+function clearGrid() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.remove());
 }
 
 createGrid(16);
